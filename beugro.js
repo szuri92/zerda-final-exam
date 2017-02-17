463d548d5c23b29ffd78cdce6bc19dcaab3dd680
Szurovecz Máté <szuri92@gmail.com>
2017-02-17 09:59:24 +0100

    Update beugro.js

--- beugro.js
+++ beugro.js
@@ -1,13 +1,4 @@
 // Create a function that takes a list of numbers and returns a new list where all the duplicate values are removed
-//
-// var filterDuplicates = function (listOfNumbers) {
-//   var filteredArray = [];
-//   for (var i = 0; i < listOfNumbers.length; i++ ) {
-//       }
-//     }
-//
-
-//   return filteredArray;
 
 function filterDuplicates(listOfNumbers) {
   var first = {};
